2cf623259e1dd561ec8d875416206055a5cdd7ee
Alex King <ark22@students.uwf.edu>
2018-11-16 14:10:34 -0600

    "update to the tutorial"

---
+++ pythonPractice.py
@@ -0,0 +1,14 @@
+#weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+class NewClass
+   def __init__(self, name):
+    self.__name=name
+
+   def getName(self)
+     return self.__name
+
+    def functionB(self, x, y):
+        return x + y
+def main():
+    myClass= NewClass("My new Class")
+    print(myClass.getName())
+main()
